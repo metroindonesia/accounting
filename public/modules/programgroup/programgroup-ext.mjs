ae5fe4efd01a5d123600a5fea8f05911be221b9b
agung-transfashion <agung@transfashionindonesia.com>
2026-02-03 18:03:33 +0700

    Implement program group hierarchy management with API extenders for path/level updates, parent selection criteria, and UI adjustments for hierarchical display.

--- public/modules/programgroup/programgroup-ext.mjs
+++ public/modules/programgroup/programgroup-ext.mjs
@@ -2,37 +2,36 @@ import Context from './programgroup-context.mjs'
 
 export async function init(self, args) {
 	console.log('initializing programgroupExtender ...')
+}
 
-	// tambahkan extender inisiasi module programgroup
 
 
-	/* // contoh menambahkan content dari template extender
-	{
-		const target = secRec.querySelector('#fRecord-section div[name="column"][exteder]')
-		const tpl = document.getElementById('tpl-record-panel')
-		if (tpl!=null) {
-			const clone = tpl.content.cloneNode(true); // salin isi template
-			target.prepend(clone)
-		}
+
+export function obj_programgroup_parent_selecting_criteria(self, obj_programgroup_parent, frm, criteria, sort, evt) {
+	// hanya yang parent akan dimunculkan
+
+	criteria.programgroup_isparent = true
+	criteria.exclude_self = frm.Inputs['programgroupHeaderEdit-obj_programgroup_id'].value
+
+	console.log('criteria', criteria)
+}
+
+
+export function headerList_addTableEvents(self, tbl) {
+	tbl.addEventListener('rowrender', async evt => { tbl_headerListRowRender(self, evt) })
+}
+
+function tbl_headerListRowRender(self, evt) {
+	const tr = evt.detail.tr
+	const td_level = tr.querySelector('td[data-name="programgroup_level"]')
+	const td_name = tr.querySelector('td[data-name="programgroup_name"]')
+
+	const level = Number(td_level.getAttribute('data-value'))
+	console.log(`${td_name.innerHTML} ${level}`)
+	if (level > 1) {
+		const paddingLeft = (level - 1) * 30
+		td_name.style.paddingLeft = `${paddingLeft}px`
 	}
-	*/	
-
-
-	
-	/* // contoh menambahkan custom validator
-	// pada html, tambahkan validator="cobaFunction:paramValue"
-	const frm = self.Modules.coaHeaderEdit.getHeaderForm()
-	const obj_coa_normal = frm.Inputs['coaHeaderEdit-obj_coa_normal']
-	$validators.addCustomValidator('cobaFunction', (v, param)=>{
-	 	console.log(v)
-	 	setTimeout(()=>{
-	 		obj_coa_normal.setError('ini error')
-	 	}, 500)
-	})	
-
-
-	*/
-
 
 }
 
