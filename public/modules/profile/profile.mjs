59dbf5bf037eea357e9f4e470cb510b9fa544cff
agung-transfashion <agung@transfashionindonesia.com>
2026-06-02 15:46:31 +0700

    buat halaman profile

--- public/modules/profile/profile.mjs
+++ public/modules/profile/profile.mjs
@@ -1,13 +1,13 @@
 const app = new $fgta5.Application('mainapp')
 const urlDir = 'public/modules/role'
-
+const form = new $fgta5.Form('user-profile-form')
 
 export default class extends Module {
 	constructor() {
 		super()
 	}
 
-	async main(args={}) {
+	async main(args = {}) {
 		console.log('initializing profile program')
 		app.setTitle('Profile')
 
